52500ffe6a56eeb0684a0070bc65c8eab925e521
naman-sriv <82610773+naman-sriv@users.noreply.github.com>
2025-01-18 15:21:36 +0530

    Add files via upload

---
+++ Weight converter/Exercise.js
@@ -0,0 +1,4 @@
+var pound = prompt("what is the weight you want to convert in pounds?");
+var pound_to_kg = pound * 0.454;
+alert("Weight in kg : "+ pound_to_kg);
+console.log("Conversion Completed");
