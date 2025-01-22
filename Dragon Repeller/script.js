f7b8792cc219cf183374c223a16fce364c077f4a
Naman Srivastava <namansriv94@gmail.com>
2025-01-22 15:04:57 +0530

    Functionality addition

--- Dragon Repeller/script.js
+++ Dragon Repeller/script.js
@@ -28,6 +28,20 @@ const weapons = [
         power:100},
 
 ];
+const monsters = [
+    {   name:"slime",
+        level:2,
+        health:15
+    },
+    {   name:"fanged beast",
+        level:8,
+        health:60
+    },
+    {   name:"dragon",
+        level:20,
+        health:300
+    },
+];
 const locations = [
     {name:"town square",
         "button text":["Go to store","Go to cave","Go to town square"],
@@ -41,6 +55,10 @@ const locations = [
         "button text":["Fight slime","Fight fanged beast","Go to town square"],
         "button functions":[fightSlime,fightBeast,goTown],
         "text":"You enter the cave. You see some monsters."},
+    {name:"fight",
+        "button text":["Attack","Dodge","Run"],
+        "button functions":[attack,dodge,goTown],
+        "text":"You are fighting a monster."}
 ];
 
 
@@ -52,15 +70,23 @@ button3.onclick = fightDragon;
 function goTown() {
     update(locations[0]);
 }
+
 function goStore() {
     update(locations[1]);
 }
+
 function goCave() {
     update(locations[2]);
 }
-function fightDragon() {
 
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display="block";
+    monsterName.innerText=monsters[fighting].name;
+    monsterHealthText.innerText=monsterHealth;
 }
+
 function buyHealth() {
     if (gold >= 10) {
         goldText.innerText = gold -= 10;
@@ -69,6 +95,7 @@ function buyHealth() {
         text.innerText = "You do not have enough gold to buy health.";
     }
 }
+
 function buyWeapon() {
     if(currentWeaponIndex < weapons.length-1) {
         if (gold >= 30) {
@@ -87,12 +114,48 @@ function buyWeapon() {
         button2.onclick = sellWeapon;
     }
 }
+
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
+
 function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+
+function attack() {
+    text.innerText="The "+monsters[fighting].name+"attacks";
+    text.innerText += " You attack it with your "+weapons[currentWeaponIndex].name+".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if(health<=0){
+        lose();
+    } else if(monsterHealth<=0){
+        defeatMonster();
+    }
+}
+
+function dodge() {
+    text.innerText = "You dodge the attack from the "+monsters[fighting].name;
+}
+
+function lose() {
 
 }
+
+function defeatMonster() {
+
+}
+
 function sellWeapon() {
     if (inventory.length > 1) {
         goldText.innerText = gold += 15;
@@ -102,6 +165,7 @@ function sellWeapon() {
         text.innerText = "Don't sell your only weapon!";
     }
 }
+
 function update(location) {
     button1.innerText = location["button text"][0];
     button1.onclick = location["button functions"][0];
